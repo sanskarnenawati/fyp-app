5a9968344dfb27beb25aa92e977b9de6140bce70
Aniket Jhariya <aniket.jhariya@gmail.com>
2024-11-12 19:02:02 +0530

    Corrects test app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App.js';
 
-test('renders learn react link', () => {
+test('renders Stock Predictor title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Stock Predictor/i);
+  expect(titleElement).toBeInTheDocument();
 });
